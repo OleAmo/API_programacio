059da61fa1bc4954947ff12da91b9e95fab049fd
Amorós Mèlich <oamoros3@edu.gencat.cat>
2026-03-24 12:41:08 +0100

    COMMIT

--- scripts/API_v1.R
+++ scripts/API_v1.R
@@ -16,5 +16,27 @@ text <- content(resposta, "text")
 dades <- fromJSON(text)
 
 dades$current_weather
-
 dades$elevation
+
+
+#  ---- CURS D'US D'API -----
+#  --------------------------
+
+#   CONCEPTES BASICS
+#   ----------------
+
+#   GET() → fa la petició
+#   content() → treu el text
+#   fromJSON() → converteix a R
+#   str() → veure estructura
+
+url <- "https://api.open-meteo.com/v1/forecast?latitude=41.38&longitude=2.17&current_weather=true"
+
+res <- GET(url)
+
+text <- content(res, "text")
+
+dades <- fromJSON(text)
+
+str(dades)
+dades$current_weather$temperature
