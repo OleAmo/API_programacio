e97f0128a171b9ec3bf7fd339672ce6b07f1ab69
Oleguer <oleguer.amoros@gmail.com>
2026-03-24 17:58:21 +0100

    commit

--- scripts/API_v1.R
+++ scripts/API_v1.R
@@ -4,26 +4,9 @@
 library(httr)
 library(jsonlite)
 
-url <- "https://api.open-meteo.com/v1/forecast"
 
-resposta <- GET(url, query = list(
-  latitude = 41.38,
-  longitude = 2.17,
-  current_weather = TRUE
-))
-
-text <- content(resposta, "text")
-dades <- fromJSON(text)
-
-dades$current_weather
-dades$elevation
-
-
-#  ---- CURS D'US D'API -----
-#  --------------------------
-
-#   CONCEPTES BASICS
-#   ----------------
+#  ---- CONCEPTES BASICS ------
+#  ----------------------------
 
 #   GET() → fa la petició
 #   content() → treu el text
@@ -34,8 +17,14 @@ dades$elevation
 #     -) R envia una petició HTTP a l’API
 #     -) L’API respon amb dades (normalment JSON)
 
-url <- "https://api.open-meteo.com/v1/forecast?latitude=41.38&longitude=2.17&current_weather=true"
-res <- GET(url)
+url <- "https://api.open-meteo.com/v1/forecast"
+
+res <- GET(url, query = list(
+  latitude = 41.38,
+  longitude = 2.17,
+  current_weather = TRUE
+))
+
 
 #   CONTENT =
 #     -) agafa el cos de la resposta
@@ -50,5 +39,50 @@ text <- content(res, "text")
 
 dades <- fromJSON(text)
 
+#   ESTRUCTURA DADES
+#   Em diu com estan estructurades:
+
 str(dades)
-dades$current_weather$temperature
+
+#   Puc TROBAR una dada concreta:
+#   Exemple = LA TEMPERATURA
+
+temp <- dades$current_weather$temperature
+wind <- dades$current_weather$windspeed
+dia <- dades$current_weather$time
+
+temp_units <- dades$current_weather_units$temperature
+wind_units <- dades$current_weather_units$windspeed
+dia_units <- dades$current_weather_units$time
+
+str_split(dia_units, "T")
+
+frase <- paste('La Temperatura és de ',temp,temp_units,
+               ', el vent es de ',wind,wind_units,
+               'en el dia ',dia)
+  
+print(frase)
+
+#  ---- OBTENIR UN SEGUIT DE DADES ------
+#  --------------------------------------
+
+#   L'API OPEN METO permet OBTENIR MÉS DADES
+#   Puc OBTENIR
+#     Per un intervar de dies
+#     Les dades de cada hora
+
+
+
+res <- GET(
+  "https://api.open-meteo.com/v1/forecast",
+  query = list(
+    latitude = 41.38,
+    longitude = 2.17,
+    start_date = "2024-03-01",
+    end_date = "2024-03-01",
+    hourly = "temperature_2m"
+  )
+)
+
+text <- content(res, "text")
+dades <- fromJSON(text)
